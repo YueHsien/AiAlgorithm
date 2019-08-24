3b81477029141c0a9957a2f7f24feb38f3c98bf8
qwer6407 <104021021@live.asia.com.tw>
2019-08-24 14:28:26 +0800

    ss

--- python/test.py
+++ python/test.py
@@ -1,2 +1,9 @@
-x=1
-print(x)
+import random
+import itertools
+import math
+import copy
+
+x1=-0.0009654754229735385
+x2=0.001198861676939389
+Z=math.pow(x1,2)+(2*math.pow(x2,2))-(0.3*(math.cos(3*math.pi*x1)))-(0.4*math.cos(4*math.pi*x2))+0.7
+print(Z)
